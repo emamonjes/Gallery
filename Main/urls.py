6d36c69319320a8f1193d6b714f580982a995307
Emanuel <manumonjes@gmail.com>
2023-11-01 19:37:31 -0300

    '#'

--- Main/urls.py
+++ Main/urls.py
@@ -16,8 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from Galeria import views 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Register/', views.Crear_Usuario)
 ]
