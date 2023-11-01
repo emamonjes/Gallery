0e708463b02a58d4975a0553fc29f27bff1f0d0b
Emanuel <manumonjes@gmail.com>
2023-11-01 19:43:16 -0300

    'Add urls, views, form user, html'

--- Galeria/views.py
+++ Galeria/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .forms import UsuarioForm
 
-# Create your views here.
+def crear_usuario(request):
+    if request.method == 'POST':
+        form = UsuarioForm(request.POST)
+        if form.is_valid():
+            # Guardar el usuario en la base de datos
+            nuevo_usuario = form.save()
+            # Realizar acciones adicionales si es necesario
+    else:
+        form = UsuarioForm()
+
+    return render(request, 'Crear_Usuario.html', {'form': form})
--- Main/urls.py
+++ Main/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Galeria import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/',views.crear_usuario)
 ]
